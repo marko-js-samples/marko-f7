2563846b4228d8a26d034cf0ec28efd782da335c
Rajat Talwar <cuterajat26@gmail.com>
2017-05-05 11:14:50 +0530

    add github url for resources

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ require('lasso').configure({
     "fileWriter": {
         "outputDir": `${directoryName}/static`,
         "fingerprintsEnabled": isProd,
-        "urlPrefix": "/static/"
+        "urlPrefix": "https://rtalwar26.github.io/marko-f7/public/static/"
     },
     "minify": isProd,
     "resolveCssUrls": true,
